df4ce1fa69a9c27cf5bac0cee3285401367a2e4d
Alessio Mazzone <alessio.mazzone12345@gmail.com>
2019-02-18 15:45:03 -0500

    Finished Media class. Starting Movie class.

--- Assignment3/movies.py
+++ Assignment3/movies.py
@@ -1,6 +1,39 @@
 # Alessio Mazzone
 
 
-
 class Media:
+    # Media class constructor
+    def __init__(self, title):
+        self.title = title
+
+    # Returns the title in lower case title stripped of all
+    # special characters and spaces replaced with '-'
+    def slug(self):
+
+        # Split title by space
+        # Remove special characters
+        # Join words with dashes
+
+        words = self.title.split()
+
+        for i in range(0,len(words)):
+            word = words[i]
+            word = ''.join(filter(str.isalnum, word))
+            words[i] = word.lower()
+
+        return '-'.join(words)
+
+class Movie(Media):
+    def __init__(self, title): 
+        super().__init__(title)
     
+    
+test1 = Media("Don't Tell Mom the Babysitter's Dead")
+test2 = Media("G.I. Joe")
+
+print(test1.slug())
+print(test2.slug())
+
+test3 = Movie("The Lord of @!&#(*@&#)(*! the rings.")
+
+print(test3.title)
