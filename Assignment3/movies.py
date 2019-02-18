852a399f3117a88ca70e263cf8bfd2436a4cdf1c
Alessio Mazzone <alessio.mazzone12345@gmail.com>
2019-02-18 16:52:56 -0500

    Finished Movie class and methods.

--- Assignment3/movies.py
+++ Assignment3/movies.py
@@ -26,6 +26,25 @@ class Media:
 class Movie(Media):
     def __init__(self, title): 
         super().__init__(title)
+
+    def Year(self, year):
+        self.year = year
+    
+    def Director(self, director):
+        self.director = director
+
+    def Runtime(self, runtime):
+        self.runtime = runtime
+
+    def abbreviation(self):
+        return "".join(super().slug().split("-"))[:3]
+        
+
+    def __repr__(self):
+        return "<Movie: " + self.title + ">"
+
+    def __str__(self):
+        return "(" + str(self.year) + ") " + self.title 
     
     
 test1 = Media("Don't Tell Mom the Babysitter's Dead")
@@ -35,5 +54,12 @@ print(test1.slug())
 print(test2.slug())
 
 test3 = Movie("The Lord of @!&#(*@&#)(*! the rings.")
-
 print(test3.title)
+
+test4 = Movie("G.I. Joe")
+test4.Year(1980)
+test4.Director("Charles Darwin")
+test4.Runtime(14.223)
+
+print(test4)
+print(test4.abbreviation())
