bf6837fb0552147b3213f413e557e8ebdb5127aa
Alessio Mazzone <alessio.mazzone12345@gmail.com>
2019-03-26 20:55:14 -0400

    Added confirm and cancel buttons. Must configure now.

--- Assignment4/salon.py
+++ Assignment4/salon.py
@@ -148,10 +148,10 @@ def user_not_found_page():
     return render_template("wrong_info.html")
 
 # Renders book appointment page
-@app.route("/book-appointment-page/<patron>-<stylist>", methods=["GET"])
-def book_appointment_page(patron, stylist):
+@app.route("/book-appointment-page/<patron>-<stylist>-<day>-<time>", methods=["GET"])
+def book_appointment_page(patron, stylist, day, time):
     # Send to appointment booking page
-    return render_template("book_appointment.html", patron=patron, stylist=stylist)
+    return render_template("book_appointment.html", patron=patron, stylist=stylist, day=day, time=time)
 
 # Renders a logged in patron's page
 @app.route("/patron-logged-in-page/<patron>")
